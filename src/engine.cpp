92e2fcb36bc9c3d3b17a7f6616fec32e86a58809
GreatBrandon <brandon2005j@gmail.com>
2026-08-24 23:19:35 +0100

    Progress on pre-generating moves Finished pawn and king basic moves Refactored knight code TODO next: en passant and castling

--- src/engine.cpp
+++ src/engine.cpp
@@ -11,39 +11,41 @@ map<string, Move> Engine::generateLegalMoves(array<array<char, 8>, 8> board, boo
     for (int row = 0; row < 8; row++) {
         for (int col = 0; col < 8; col++) {
             char piece = board[row][col];
+            this->sRow = row;
+            this->sCol = col;
             if (white and isWhitePiece(piece)) {
                 if (piece == White::KING) {
-
+                    generateLegalMovesKing();
                 } else if (piece == White::QUEEN) {
-                    generateLegalMovesRow(row, col);
-                    generateLegalMovesCol(row, col);
-                    generateLegalMovesDiagonal(row, col);
+                    generateLegalMovesRow();
+                    generateLegalMovesCol();
+                    generateLegalMovesDiagonal();
                 } else if (piece == White::BISHOP) {
-                    generateLegalMovesDiagonal(row, col);
+                    generateLegalMovesDiagonal();
                 } else if (piece == White::KNIGHT) {
-                    generateLegalMovesKnight(row, col);
+                    generateLegalMovesKnight();
                 } else if (piece == White::ROOK) {
-                    generateLegalMovesRow(row, col);
-                    generateLegalMovesCol(row, col);
+                    generateLegalMovesRow();
+                    generateLegalMovesCol();
                 } else {
-
+                    generateLegalMovesPawn();
                 }
             } else if (!white and isBlackPiece(piece)) {
                 if (piece == Black::KING) {
-
+                    generateLegalMovesKing();
                 } else if (piece == Black::QUEEN) {
-                    generateLegalMovesRow(row, col);
-                    generateLegalMovesCol(row, col);
-                    generateLegalMovesDiagonal(row, col);
+                    generateLegalMovesRow();
+                    generateLegalMovesCol();
+                    generateLegalMovesDiagonal();
                 } else if (piece == Black::BISHOP) {
-                    generateLegalMovesDiagonal(row, col);
+                    generateLegalMovesDiagonal();
                 } else if (piece == Black::KNIGHT) {
-                    generateLegalMovesKnight(row, col);
+                    generateLegalMovesKnight();
                 } else if (piece == Black::ROOK) {
-                    generateLegalMovesRow(row, col);
-                    generateLegalMovesCol(row, col);
+                    generateLegalMovesRow();
+                    generateLegalMovesCol();
                 } else {
-
+                    generateLegalMovesPawn();
                 }
             }
         }
@@ -56,77 +58,130 @@ map<string, Move> Engine::generateLegalMoves(array<array<char, 8>, 8> board, boo
 }
 
 // TODO add check and mate for all below
-void Engine::generateLegalMovesRow(int sRow, int sCol) {
+void Engine::generateLegalMovesRow() {
     for (int i = sCol + 1; i < 8; i++) {
-        if (addMove(sRow, sCol, sRow, i)) break;
+        if (addMove(sRow, i)) break;
     }
     for (int i = sCol - 1; i >= 0; i--) {
-        if (addMove(sRow, sCol, sRow, i)) break;
+        if (addMove(sRow, i)) break;
     }
 }
 
-void Engine::generateLegalMovesCol(int sRow, int sCol) {
+void Engine::generateLegalMovesCol() {
     for (int i = sRow + 1; i < 8; i++) {
-        if (addMove(sRow, sCol, i, sCol)) break;
+        if (addMove(i, sCol)) break;
     }
     for (int i = sRow - 1; i >= 0; i--) {
-        if (addMove(sRow, sCol, i, sCol)) break;
+        if (addMove(i, sCol)) break;
     }
 }
 
-void Engine::generateLegalMovesDiagonal(int sRow, int sCol) {
+void Engine::generateLegalMovesDiagonal() {
     for (int col = sCol + 1, row = sRow - 1; col < 8 && row >= 0; col++, row--) {
-        if (addMove(sRow, sCol, row, col)) break;
+        if (addMove(row, col)) break;
     }
     for (int col = sCol + 1, row = sRow + 1; col < 8 && row < 8; col++, row++) {
-        if (addMove(sRow, sCol, row, col)) break;
+        if (addMove(row, col)) break;
     }
     for (int col = sCol - 1, row = sRow - 1; col >= 0 && row >= 0; col--, row--) {
-        if (addMove(sRow, sCol, row, col)) break;
+        if (addMove(row, col)) break;
     }
     for (int col = sCol - 1, row = sRow + 1; col >= 0 && row < 8; col--, row++) {
-        if (addMove(sRow, sCol, row, col)) break;
+        if (addMove(row, col)) break;
     }
 }
 
-void Engine::generateLegalMovesKnight(int sRow, int sCol) {
-    if (sCol - 2 >= 0) {
-        if (sRow - 1 >= 0) addMove(sRow, sCol, sRow - 1, sCol - 2);
-        if (sRow + 1 < 8) addMove(sRow, sCol, sRow + 1, sCol - 2);
-    }
-    if (sCol + 2 < 8) {
-        if (sRow - 1 >= 0) addMove(sRow, sCol, sRow - 1, sCol + 2);
-        if (sRow + 1 < 8) addMove(sRow, sCol, sRow + 1, sCol + 2);
-    }
-    if (sCol - 1 >= 0) {
-        if (sRow - 2 >= 0) addMove(sRow, sCol, sRow - 2, sCol - 1);
-        if (sRow + 2 < 8) addMove(sRow, sCol, sRow + 2, sCol - 1);
-    }
-    if (sCol + 1 < 8) {
-        if (sRow - 2 >= 0) addMove(sRow, sCol, sRow - 2, sCol + 1);
-        if (sRow + 2 < 8) addMove(sRow, sCol, sRow + 2, sCol + 1);
+void Engine::generateLegalMovesKnight() {
+    generateLegalMovesUsingOffsets({ {
+        {-2, -1}, {-2, 1},
+        {-1, -2}, {-1, 2},
+        {1, -2}, {1, 2},
+        {2, -1}, {2, 1}
+    } });
+}
+
+void Engine::generateLegalMovesKing() {
+    generateLegalMovesUsingOffsets({ {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1}, {0, 1},
+        {1, -1}, {1, 0}, {1, 1},
+    } });
+    // TODO castling
+}
+
+void Engine::generateLegalMovesPawn() {
+    // TODO en passant
+    if (isWhite) {
+        if (board[sRow - 1][sCol] == ' ') {
+            addMove(sRow - 1, sCol);
+            if (sRow == 6 && board[sRow - 2][sCol] == ' ') {
+                addMove(sRow - 2, sCol);
+            }
+        }
+        if (sCol - 1 >= 0 && isBlackPiece(board[sRow - 1][sCol - 1])) addMove(sRow - 1, sCol - 1);
+        if (sCol + 1 < 8 && isBlackPiece(board[sRow - 1][sCol + 1])) addMove(sRow - 1, sCol + 1);
+    } else {
+        if (board[sRow + 1][sCol] == ' ') {
+            addMove(sRow + 1, sCol);
+            if (sRow == 1 && board[sRow + 2][sCol] == ' ') {
+                addMove(sRow + 2, sCol);
+            }
+        }
+        if (sCol - 1 >= 0 && isWhitePiece(board[sRow + 1][sCol - 1])) addMove(sRow + 1, sCol - 1);
+        if (sCol + 1 < 8 && isWhitePiece(board[sRow + 1][sCol + 1])) addMove(sRow + 1, sCol + 1);
     }
 }
 
-bool Engine::addMove(int sRow, int sCol, int eRow, int eCol) {
+void Engine::generateLegalMovesUsingOffsets(array<pair<int, int>, 8> offsets) {
+    for (auto [oRow, oCol] : offsets) {
+        int eRow = sRow + oRow;
+        int eCol = sCol + oCol;
+
+        if (eRow >= 0 && eRow < 8 && eCol >= 0 && eCol < 8) addMove(eRow, eCol);
+    }
+}
+
+bool Engine::addMove(int eRow, int eCol) {
     char end = board[eRow][eCol];
     if ((isWhite && isWhitePiece(end)) || (!isWhite && isBlackPiece(end))) return true;
+    bool isPawn = board[sRow][sCol] == White::PAWN || board[sRow][sCol] == Black::PAWN;
     string move = string();
 
-    move += toupper(board[sRow][sCol]);
+    if (!isPawn) {
+        move += toupper(board[sRow][sCol]);
+    }
 
     if (board[eRow][eCol] != ' ') {
-        move += 'x';
+        if (isPawn) {
+            move = move + (char)('a' + sCol) + 'x';
+        } else {
+            move += 'x';
+        }
     }
 
     move = move + (char)('a' + eCol) + (char)('8' - eRow);
+
+    if (isPawn && ((isWhite && eRow == 0) || (!isWhite && eRow == 7))) {
+        array<string, 4> promotionMoves = { {
+            move + "=Q", move + "=B", move + "=N", move + "=R"
+            } };
+        for (auto& m : promotionMoves) {
+            bool conflict = legalMoves.find(m) != legalMoves.end();
+            if (conflict) {
+                cout << "Disambiguate move: " << m << endl;
+            } else {
+                legalMoves[m] = Move(sRow, sCol, eRow, eCol);
+            }
+        }
+        return false;
+    }
+
     bool conflict = legalMoves.find(move) != legalMoves.end();
     if (conflict) {
         cout << "Disambiguate move: " << move << endl;
     } else {
         legalMoves[move] = Move(sRow, sCol, eRow, eCol);
     }
-    // TODO generate notation
     // TODO check
     if (end != ' ') return true;
     return false;
